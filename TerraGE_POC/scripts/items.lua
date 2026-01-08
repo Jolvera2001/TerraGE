ec20762707ee32daea7260a4a6c192a8221209a5
Johan Olvera <olvjohan2001@gmail.com>
2026-01-08 09:34:57 -0600

    more json loading

---
+++ TerraGE_POC/scripts/items.lua
@@ -0,0 +1,36 @@
+﻿local M = {}
+
+---@class Item
+---@field id string Item Id
+---@field weight number Weight of the item
+local Item = {}
+Item.__index = Item
+
+function Item.new(data)
+	---@type Item
+	local self = setmetatable({}, Item)
+	
+	self.id = data.id or "unknown"
+	self.name = data.name or "unknown"
+	self.weight = data.weight or 1.0
+	return self
+end
+
+function Item:get_weight()
+	return self.weight
+end
+
+function M.load_items()
+	local raw = engine.load_json("items.json")
+	local items = {}
+	
+	for i, item_data in ipairs(raw) do
+		local item = Item.new(item_data)
+		
+		table.insert(items, item)
+	end
+	
+	return items
+end
+
+return M
--- TerraGE_POC/scripts/main.lua
+++ TerraGE_POC/scripts/main.lua
@@ -1,7 +1,17 @@
-﻿local test = require("test")
+﻿local test = require('test')
+local items = require('items')
 
 function initialize() 
 	test.test_item_loading()
+	
+	local item_list = items.load_items()
+	
+	for i, item in ipairs(item_list) do
+		print("[LUA]: ======== ITEM LOADED ========")
+		print("[LUA]: Item id: " .. item.id)
+		print("[LUA]: Item name: " .. item.name)
+		print("[LUA]: Item weight: " .. item.weight)
+	end
 end
 
 function update()
